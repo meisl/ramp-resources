d2afe73f9fcd1265415982c12bb6b09a7a2a39b7
Matthias Kling <m.kling@sprax.eu>
2013-01-24 20:40:54 +0100

    minor refactoring + reformatting, just so that it is visible in the big diff and I can comment on it

--- test/test-helper.js
+++ test/test-helper.js
@@ -8,11 +8,11 @@ var util = require("util");
 
 function verifyResourceError(message, e) {
     if (e.name !== "InvalidResourceError") {
-        this.fail("Expected rr.createResource to fail with " +
+        B.assertions.fail("Expected rr.createResource to fail with " +
                   "InvalidResourceError, but failed with " + e.name);
     }
     if (!new RegExp(message).test(e.message)) {
-        this.fail("Expected InvalidResourceError message (" +
+        B.assertions.fail("Expected InvalidResourceError message (" +
                   e.message + ") to match " + message);
     }
     return true;
@@ -22,32 +22,40 @@ function verifyResourceError(message, e) {
 
 B.assertions.add("invalidResource", {
     assert: function (path, res, message) {
+        var ret;
         try {
             if (typeof path === "string") {
                 rr.createResource(path, res);
-                return false;
+                ret = false;
             } else {
-                var ret;
-                path.addResource(res).then(function () {}, function (err) {
-                    ret = verifyResourceError(message, err);
-                });
+                path.addResource(res).then(
+                    function () {},
+                    function (err) {
+                        ret = verifyResourceError(message, err);
+                    }
+                );
                 return ret;
             }
         } catch (e) {
-            return verifyResourceError(message, e);
+            ret = verifyResourceError(message, e);
         }
+        return ret;
     },
     assertMessage: "Expected to fail"
 });
 
 B.assertions.add("content", {
     assert: function (resource, expected, done) {
-        resource.content().then(done(function (actual) {
-            assert.same(actual, expected);
-        }), done(function (err) {
-            buster.log(err.stack);
-            B.assertions.fail("content() rejected");
-        }));
+        resource.content().then(
+            done(function (actual) {
+                assert.same(actual, expected);
+                // why doesn't B.assertions.same work?
+            }),
+            done(function (err) {
+                buster.log(err.stack);
+                B.assertions.fail("content() rejected");
+            })
+        );
         return true;
     }
 });
@@ -60,10 +68,11 @@ B.assertions.add("resourceEqual", {
             B.assertions.deepEqual(res1.headers(), res2.headers());
         if (!equal) { return false; }
 
-        when.all([res1.content(), res2.content()]).then(function (contents) {
-            assert.equals(contents[0], contents[1]);
-            done();
-        });
+        when.all([res1.content(), res2.content()]).then(
+            done(function (contents) {
+                B.assertions.equals(contents[0], contents[1]);
+            })
+        );
         return true;
     },
     assertMessage: "Expected resources ${0} and ${1} to be the same"
@@ -146,6 +155,7 @@ function req(opt, callback) {
     return resultReq;
 }
 
+// maybe rename to serverSetUp? there's serverTearDown, at last
 function createServer(middleware, done) {
     var server = http.createServer(function (req, res) {
         if (!middleware.respond(req, res)) {
@@ -162,6 +172,7 @@ function serverTearDown(done) {
     this.server.close();
 }
 
+// would be nicer to have it just like createServer / serverTearDown
 function createProxyBackend(port) {
     var backend = { requests: [] };
 
@@ -175,7 +186,7 @@ function createProxyBackend(port) {
     });
     server.listen(port);
 
-    backend.close = function (done) {
+    backend.close = function (done) { // to be called in tearDown
         var i, l;
         for (i = 0, l = backend.requests.length; i < l; ++i) {
             if (!backend.requests[i].res.ended) {
